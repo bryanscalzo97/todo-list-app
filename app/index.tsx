eaa47bd64189479abeb1cf42daf4b2fd87ed4367
Bryan Scalzo <bryan@bantr.community>
2025-01-19 10:13:52 -0500

    feat: implement login form and todo list navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,41 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, SafeAreaView, TextInput, Button } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
 const Login = () => {
+  const router = useRouter();
+  const { control, handleSubmit } = useForm<FormData>();
+
+  type FormData = {
+    email: string;
+    password: string;
+  };
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+    // Handle form submission
+  };
+
   return (
     <SafeAreaView>
       <View>
-        <Text>index</Text>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput placeholder="Email" onChangeText={onChange} value={value} />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextInput placeholder="Password" secureTextEntry onChangeText={onChange} value={value} />
+          )}
+        />
+        <Button title="Login" onPress={handleSubmit(onSubmit)} />
+        <Button title="Go to Signup" onPress={() => router.push('/signup')} />
       </View>
     </SafeAreaView>
   );
--- app/todos/index.tsx
+++ app/todos/index.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
 
 const TodoList = () => {
+  const router = useRouter();
+
   return (
     <SafeAreaView>
       <View>
-        <Text>List of todos</Text>
+        {/* TODO: Add dynamic data */}
+        <Text> Todo List:</Text>
+        <Text> 1. Clean the house</Text>
+        <Text> 2. Cook</Text>
+        <Button title="Add Todo" onPress={() => router.push('/todos/addTask')} />
       </View>
     </SafeAreaView>
   );
